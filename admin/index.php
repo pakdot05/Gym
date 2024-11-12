7e672e1cd5005c69ed44d07194172e07e4184d0c
pakdot05 <125708652+pakdot05@users.noreply.github.com>
2024-11-12 16:46:47 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,11 @@
 <?php
-        if (isset($_POST['login'])) {
-            $frm_data = filteration($_POST);
-            
-            $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
-            $values = [$frm_data['admin_name'], $frm_data['admin_pass']];
-
-            $res = select($query, $values, "ss");
-            if ($res->num_rows == 1) {
-                $row = mysqli_fetch_assoc($res);
-                $_SESSION['adminLogin'] = true;
-                $_SESSION['adminId'] = $row['admin_id'];
-                redirect('dashboard.php');
-            } else {
-                alert('error', 'Login Unsuccessful!');
-            }
-        }
-    ?>
-    <?php require('inc/scripts.php'); ?>
+    require('inc/essentials.php');
+    require('inc/db_config.php');
+    session_start();
+    if((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
+        redirect('dashboard.php');
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,16 +16,20 @@
     <?php require('inc/links.php'); ?>
 
     <style>
-        div.login-form {
+        div.login-form{
+
             position: absolute;
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
             width: 400px;
+
         }
     </style>
+
 </head>
 <body class="bg-light">
+
     <div class="login-form text-center rounded bg-white shadow overflow-hidden">
         <form method="POST">
             <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
@@ -51,5 +44,30 @@
             </div>
         </form>
     </div>
+
+    <?php
+
+        if(isset($_POST['login']))
+        {
+            $frm_data = filteration($_POST);
+            
+            $query = "SELECT * FROM   `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
+            $values = [$frm_data['admin_name'],$frm_data['admin_pass']];
+
+            $res = select($query,$values,"ss");
+            if($res->num_rows==1){
+                $row = mysqli_fetch_assoc($res);
+                $_SESSION['adminLogin'] = true;
+                $_SESSION['adminId'] = $row['admin_id'];
+                redirect('dashboard.php');
+            }
+            else{
+                alert('error','Login Unsucessful!');
+            }
+        }
+    
+    ?>
+  
+    <?php require('inc/scripts.php'); ?>
 </body>
 </html>
