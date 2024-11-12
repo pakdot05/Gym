ca14385b5121cca048548939ed240c0c570eeab0
pakdot05 <125708652+pakdot05@users.noreply.github.com>
2024-11-12 15:44:13 +0800

    Update essentials.php

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,11 +1,11 @@
 <?php
-// Start session at the very beginning of the file
+// Start session at the very beginning of the file, ensuring no whitespace or output before this
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
 // Define constants
-define('SITE_URL', 'http://hkc0s08c4so4k8k0ck8wk8gs.146.190.103.211.sslip.io');
+define('SITE_URL', 'http://hkc0s08c4so4k8k0ck8wk8gs.146.190.103.211.sslip.io/');
 define('ABOUT_IMG_PATH', SITE_URL . 'images/about/');
 define('CAROUSEL_IMG_PATH', SITE_URL . 'images/carousel/');
 define('TRAINORS_IMG_PATH', SITE_URL . 'images/trainors/');
@@ -23,12 +23,13 @@ function adminLogin() {
     }
 }
 
-// Other utility functions
+// Redirect function
 function redirect($url) {
     echo "<script>window.location.href='$url';</script>";
     exit;
 }
 
+// Alert function
 function alert($type, $msg) {
     $bs_class = ($type === "success") ? "alert-success" : "alert-danger";
     echo <<<alert
@@ -38,4 +39,3 @@ function alert($type, $msg) {
         </div>
     alert;
 }
-?>
